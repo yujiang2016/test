f81d548aacab82ca63773cea81dde7a0e481681d
yujiang <935315164@qq.com>
2016-09-13 19:56:54 +0800

    ceshi

---
+++ app/controller/User.php
@@ -0,0 +1,15 @@
+<?php namespace app\controller;
+
+/**
+ * Created by PhpStorm.
+ * User: Administrator
+ * Date: 2016/9/13
+ * Time: 19:25
+ */
+
+class User
+{
+    public function show(){
+        echo 888;
+    }
+}
---
+++ app/index.php
@@ -0,0 +1,6 @@
+<?php
+
+require 'vendor/autoload.php';
+use app\controller\user;
+$obj=new User;
+$obj->show();
